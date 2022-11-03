99e7ab507ee28735799b29d576ef2b6c3db9d3f0
Greg Munro <gregmunro90@gmail.com>
2022-11-03 18:29:05 +0100

    mob res fix

--- src/pages/clients.js
+++ src/pages/clients.js
@@ -8,16 +8,16 @@ const clients = () => {
       <Navbar />
       <div className='client-cards'>
         {/* 1 */}
-        <div class='container'>
-          <div class='card'>
-            <div class='imgBx'>
+        <div className='container'>
+          <div className='card'>
+            <div className='imgBx'>
               <img src='https://i.postimg.cc/VNbPv3Fq/Screenshot-2022-11-03-at-16-11-30.png' />
             </div>
-            <div class='contentBx'>
+            <div className='contentBx'>
               <h2>Greg Munro</h2>
               <h5>frontend developer, barcelona</h5>
-              <div class='size'></div>
-              <div class='color'></div>
+              <div className='size'></div>
+              <div className='color'></div>
               <a href='https://greg-munro.com/' target='_blank'>
                 visit
               </a>
@@ -25,16 +25,16 @@ const clients = () => {
           </div>
         </div>
         {/* 2 */}
-        <div class='container-1'>
-          <div class='card-1'>
-            <div class='imgBx-1'>
+        <div className='container-1'>
+          <div className='card-1'>
+            <div className='imgBx-1'>
               <img src='https://tinypic.host/images/2022/11/02/Screenshot-2022-11-02-at-12.02.39.png' />
             </div>
-            <div class='contentBx-1'>
+            <div className='contentBx-1'>
               <h2>Berghain Bootcamp</h2>
               <h5>quiz game based on techno music</h5>
-              <div class='size-1'></div>
-              <div class='color-1'></div>
+              <div className='size-1'></div>
+              <div className='color-1'></div>
               <a href='https://gmunro90.github.io/Berghain-Bootcamp/' target='_blank'>
                 visit
               </a>
@@ -42,16 +42,16 @@ const clients = () => {
           </div>
         </div>
         {/* 3 */}
-        <div class='container-2'>
-          <div class='card-2'>
-            <div class='imgBx-2'>
+        <div className='container-2'>
+          <div className='card-2'>
+            <div className='imgBx-2'>
               <img src='https://i.postimg.cc/VLQ6thRR/Screenshot-2022-11-02-at-12-15-13.png' />
             </div>
-            <div class='contentBx-2'>
+            <div className='contentBx-2'>
               <h2>Food Talleres</h2>
               <h5>cooking classes website, barcelona</h5>
-              <div class='size-2'></div>
-              <div class='color-2'></div>
+              <div className='size-2'></div>
+              <div className='color-2'></div>
               <a href='https://talleres-barcelona-food.netlify.app/' target='_blank'>
                 visit
               </a>
